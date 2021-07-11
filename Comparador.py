148a9ac41bd4618b7c3fe98ffc48832f1942d08d
KarolQuilachamin <87280219+KarolQuilachamin@users.noreply.github.com>
2021-07-11 14:29:21 -0500

    Add files via upload

--- Comparador.py
+++ Comparador.py
@@ -26,17 +26,17 @@ h.close()
 a=open("FINALDOC.txt","w")
 for i in range(len(day)):
     if (day[i]=="Monday") and (plate[i]=="1" or plate[i]=="2") and (hour[i]=="1"):
-        a.write('1'+'\n')
+        a.write('SE ENCUENTRA EN HORARIO DE PICO Y PLACA'+'\n')
     elif (day[i]=="Tuesday") and (plate[i]=="3" or plate[i]=="4") and (hour[i]=="1"):
-        a.write('1'+'\n')
+        a.write('SE ENCUENTRA EN HORARIO DE PICO Y PLACA'+'\n')
     elif (day[i]=="Wednesday") and (plate[i]=="5" or plate[i]=="6") and (hour[i]=="1"):
-        a.write('1'+'\n')
+        a.write('SE ENCUENTRA EN HORARIO DE PICO Y PLACA'+'\n')
     elif (day[i]=="Thursday") and (plate[i]=="7" or plate=="8") and (hour[i]=="1"):
-        a.write('1'+'\n')
+        a.write('SE ENCUENTRA EN HORARIO DE PICO Y PLACA'+'\n')
     elif (day[i]=="Friday") and (plate[i]=="9" or plate[i]=="0") and (hour[i]=="1"):
-        a.write('1'+'\n')
+        a.write('SE ENCUENTRA EN HORARIO DE PICO Y PLACA'+'\n')
     else:
-        a.write('0'+'\n')
+        a.write('No está en horario de Pico y Placa'+'\n')
 a.close()
         
 
